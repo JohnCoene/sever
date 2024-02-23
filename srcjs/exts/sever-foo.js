9a173b5a9773b421aa28308bddc772990d8c3248
John Coene <jcoenep@gmail.com>
2024-02-23 09:56:26 +0100

    fix: box supports Bootstrap 5 fixes #18

--- srcjs/exts/sever-foo.js
+++ srcjs/exts/sever-foo.js
@@ -1,39 +1,45 @@
 const changeDefault = (opts) => {
   // set css
   let css,
-    head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
-  
+    head = document.head || document.getElementsByTagName("head")[0],
+    style = document.createElement("style");
+
   let bgColor = opts.bg_color;
 
-  if(bgColor == undefined || bgColor == null)
-    bgColor = "none";
+  if (bgColor == undefined || bgColor == null) bgColor = "none";
 
-  css = "#shiny-disconnected-overlay{background-color: " + bgColor + "; opacity: " + opts.opacity + "; background-size: cover; background-image: url('" + opts.bg_image + "')}";
+  css =
+    "#shiny-disconnected-overlay{background-color: " +
+    bgColor +
+    "; opacity: " +
+    opts.opacity +
+    "; background-size: cover; background-image: url('" +
+    opts.bg_image +
+    "')}";
 
   head.appendChild(style);
 
-  if (style.styleSheet){
+  if (style.styleSheet) {
     // This is required for IE8 and below.
     style.styleSheet.cssText = css;
   } else {
     style.appendChild(document.createTextNode(css));
   }
-}
+};
 
 const sever = (opts) => {
   // declare
   var classes = "severed";
   var dv = document.createElement("DIV"),
-      parent = document.createElement("DIV");
-  
+    parent = document.createElement("DIV");
+
   // insert content
   dv.innerHTML = opts.content;
 
   // change classes if is box
-  if(opts.box){
-    classes += " panel";
-    dv.className = "panel-body";
+  if (opts.box) {
+    classes += " panel card";
+    dv.className = "panel-body card-body";
   }
 
   parent.className = classes;
@@ -48,17 +54,18 @@ const sever = (opts) => {
   parent.appendChild(dv);
 
   document.body.appendChild(parent);
-}
+};
 
 const removeSever = () => {
   let severed = document.getElementsByClassName("severed");
   for (let s of severed) {
     s.remove();
   }
-}
+};
 
 export const handleSever = (opts) => {
-	removeSever();
-	changeDefault(opts);
-	sever(opts);
-}
+  removeSever();
+  changeDefault(opts);
+  sever(opts);
+};
+
